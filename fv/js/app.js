3a36d18e0a6ceac1cd645ac6245c4a975875b47c
Code Beyond Work <codebeyondwork@gmail.com>
2016-05-04 19:21:39 -0400

    Just playing around with different javascript functionality.

--- fv/js/app.js
+++ fv/js/app.js
@@ -1,6 +1,33 @@
-var submitBtn = document.getElementById('calculate');
-submitBtn.onclick = function(e)
+var form = document.getElementById('fv-form');
+var msg = form.appendChild(document.createElement("p"));
+msg.appendChild(document.createTextNode("Go ahead, click me "));
+msg.appendChild(document.createElement("em")).appendChild(document.createTextNode("now!"));
+
+// create document fragment
+var f = document.createDocumentFragment();
+var q = f.appendChild(document.createElement("p"));
+q.appendChild(document.createTextNode("You know you want to, "));
+q.appendChild(document.createElement("strong")).appendChild(document.createTextNode("don't you?"));
+q.id = "fragment";
+
+form.insertBefore(q, form.firstChild);
+
+form.addEventListener("submit", validateForm);
+form.addEventListener("submit", calculateFV);
+
+function calculateFV()
 {
-    e.stopPropagation();
-    console.debug('Hey there!');
+    console.log('clicked');
+    return false;
 }
+
+function validateForm(e)
+{
+    console.log(e);
+    e.preventDefault();
+    return false;
+}
+
+// remove the form
+// form.parentNode.removeChild(form);
+
